e775d4ce59ffb7fe077edd1bf778f2164b08d585
Shafin Thiyam <shafin.thiyam@outlook.com>
2018-08-24 22:49:19 +0530

    call to function for rest

---
+++ Chap3/code/TestRestFul.py
@@ -0,0 +1,31 @@
+from flask_restful import Resource,Api
+from flask import Flask,request
+
+
+items=[]
+
+class Item(Resource):
+    def get(self,name):
+        #for item in items:
+            #item=list(filter(lambda x: x['name']==name, items))  though list will give list of item but in this case we have single item so better to use 'next'
+            # next give first item found by the filter function.
+            #next function will throw error if no item is found so we add more parameter as 'None' which is returned when no item is found.
+        item=next(filter(lambda x: x['name']==name, items),None)
+
+            #if item['name']==name:
+        return {'item':item}, 200 if item is not None else 404
+        #return {"Item" :'none'},404 #404 status code os for file not found and most common status code is 200 which is for 'ok'
+
+    def post(self,name):
+        if next(filter(lambda x:x['name']==name,items),None) is not None:
+            return {'Message':"Item with name {} is already present".format(name)}, 400 # 400 is for bad request status
+
+        data=request.get_json()
+        item={'name':name,'price':data['price']}
+        items.append(item)
+        return item,201 #201 is for created
+
+class Itemlist(Resource):
+    def get(self):
+        return {'items',items}
+
