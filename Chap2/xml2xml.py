238e502b94dacb18f9797f10a94545081e7b52a1
Shafin Thiyam <shafin.thiyam@thomsonreuters.com>
2018-08-24 09:04:59 +0530

    call to function for rest

--- Chap2/xml2xml.py
+++ Chap2/xml2xml.py
@@ -1,13 +1,14 @@
 import os;
+import sys
 
 __author__='Shafin Thiyam'
+
 class xml_processsing:
     def __init__(self,xsl,input_xml):
         self.xsl=xsl
         self.input_xml=input_xml
 
     def tranformation(self):
-        print("processing started...")
         if self.input_xml.endswith('txt'):
             input_list = open(self.input_xml).read().split("\n")
             for i in input_list:
@@ -17,6 +18,11 @@ class xml_processsing:
         else:
             print("Transforming {} to {}_output.xml".format(self.input_xml,self.xsl,self.input_xml.split('.')[0]))
             os.system("java net.sf.saxon.Transform -s:{0} -xsl:{1} -o:{2}_output.xml".format(self.input_xml,self.xsl,self.input_xml.split('.')[0]))
-        print("processing Completed")
 
-
+if __name__=='__main__':
+    print("processing started...")
+    xsl= sys.argv[1]
+    inputs=sys.argv[2]
+    transform=xml_processsing(xsl,inputs)
+    transform.tranformation()
+    print("Transformation Done")
