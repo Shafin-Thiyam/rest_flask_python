ab0965669e170936a668eac8172fc302edb2cf5c
Shafin Thiyam <shafin.thiyam@outlook.com>
2018-08-25 07:23:56 +0530

    Addition of health check

--- xmltransformer/rest.py
+++ xmltransformer/rest.py
@@ -15,7 +15,8 @@ sys.path.append('src')
 from xmlTransformer import xml_processsing
 
 urls = (
-    '/transformer/(.*)', 'Trans'
+    '/health', 'Health',
+    '/xmltransformer/(.*)', 'Trans'
 )
 app = web.application(urls, globals())
 
@@ -34,34 +35,17 @@ if len(sys.argv) > 2:
 else:
     build = 0
 
+
+class Health:
+    def GET(self):
+        return '{"description":"Python xmltransformer","status":"UP"}'
+
+
+
 class Trans:
     def POST(self,xsl):
-        out={}
+        out_data={}
         x=web.input(input_file={})
-
-        dir_path = os.path.dirname(__file__)
-
-        if 'input_file' in x:
-
-            work_dir = os.path.join(dir_path, '__work__', datetime.now().strftime("%Y%m%d%H%M%S%f") + "_" + random.choice('ABCDEFGHIJKL') )
-            if not os.path.exists(work_dir):
-                os.makedirs(work_dir)
-
-            file_path = x.input_file.filename.replace('\\', '/')  # replaces the windows-style slashes with linux ones.
-            file_name = file_path.split('/')[-1]
-            input_file = os.path.join(work_dir, file_name)
-            with open(input_file, 'wb') as saved:
-                shutil.copyfileobj(x['input_file'].file, saved)
-            xml_pro=xml_processsing(xsl,input_file)
-            xml_pro.tranformation()
-
-class Tag:
-
-    def POST(self, publication):
-
-        out_data = {}
-
-        x = web.input(input_file={})
 
         dir_path = os.path.dirname(__file__)
 
@@ -79,63 +63,46 @@ class Tag:
 
             output_file = os.path.join(work_dir, "out_" + file_name)
 
+            xml_pro=xml_processsing(xsl,input_file,output_file)
 
-
-            # python = sys.executable
-            # at_cmd = python + " src/Autotagger.py " + publication + " " + input_file + " " + output_file + " xml"
-            # print("AutoTagger : " + at_cmd + "\n")
-            # os.system(at_cmd)
-
-            tagger = AutoTagger()
-            tagger.linking(publication, input_file, output_file+"cited", 'xml')
-            Inv_CQ = CiteCleaner()
-            Inv_CQ.Invalid_CQ_clean(output_file+"cited", output_file)
-            os.remove(output_file+"cited")
+            xml_pro.tranformation()
 
             f = open(output_file, "r", encoding="utf8")
             out_data["content"] = f.read().rstrip("\n")
             f.close()
 
-            if os.path.exists(work_dir):
-                shutil.rmtree(work_dir)
-
-            return out_data["content"].encode('utf-8')
-
-
-
 def register():
     fqdn = socket.getfqdn()
-    print("\n-- autotagger service register started")
+    print("\n-- xmltransformer service register started")
     c = consul.Consul()
 
     # a HTTP GET against url every 10s for health check
     check = consul.Check.http("http://localhost:%d/health" % port, "30s")
 
-    c.agent.service.register("autotagger", "autotagger",
+    c.agent.service.register("xmltransformer", "xmltransformer",
                              check=check,address=fqdn,port=port,
-                             tags=['build='+build, "port=%d" % port])
+                             tags=["port=%d" % port])
 
     # index, nodes = c.health.service('autotagger') #
     print("services: " + str(c.agent.services()))
 
 
 def unregister():
-    print("\n-- autotagger service unregister started")
+    print("\n-- xmltransformer service unregister started")
     c = consul.Consul()
-    c.agent.service.deregister("autotagger")
+    c.agent.service.deregister("xmltransformer")
     print("services: " + str(c.agent.services()))
 
 
 def get_build():
     version = str((subprocess.check_output(['git', 'describe'])).decode("utf-8").rstrip())
 
-    build = version.replace("TRTA-ContentTechnology-autotagger-", "")
+    build = version.replace("TRTA-ContentTechnology-xmltransformer-", "")
     build = re.sub('\-[a-z0-9\-]*', '', build)
 
     return build
 
 if __name__ == "__main__":
-    #register()
+    register()
     app.run()
-    # unregister()
-
+    # unregister()  
