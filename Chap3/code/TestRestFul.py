914dca7d9b153138d2880d40be746878391358f9
Shafin Thiyam <shafin.thiyam@outlook.com>
2018-08-26 00:08:49 +0530

    python code for authentication

--- Chap3/code/TestRestFul.py
+++ Chap3/code/TestRestFul.py
@@ -6,7 +6,7 @@ from flask_jwt import JWT,jwt_required
 items=[]
 
 class Item(Resource):
-    @jwt_required
+    @jwt_required()
     def get(self,name):
         #for item in items:
             #item=list(filter(lambda x: x['name']==name, items))  though list will give list of item but in this case we have single item so better to use 'next'
@@ -15,10 +15,12 @@ class Item(Resource):
         item=next(filter(lambda x: x['name']==name, items),None)
 
             #if item['name']==name:
-        return {'item':item}, 200 if item is not None else 404
+        return {'item':item}, 200 if item is not None else 400
         #return {"Item" :'none'},404 #404 status code os for file not found and most common status code is 200 which is for 'ok'
 
+    @jwt_required()
     def post(self,name):
+
         if next(filter(lambda x:x['name']==name,items),None) is not None:
             return {'Message':"Item with name {} is already present".format(name)}, 400 # 400 is for bad request status
 
@@ -28,6 +30,7 @@ class Item(Resource):
         return item,201 #201 is for created
 
 class Itemlist(Resource):
+    @jwt_required()
     def get(self):
-        return {'items',items}
+        return {'items':items}
 
--- Chap3/code/app.py
+++ Chap3/code/app.py
@@ -4,10 +4,14 @@ from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
 import TestRestFul
 
+
 app=Flask(__name__)
-app.secret_key="haiqa"
+app.secret_key="jose"
 api=Api(app)
 jwt=JWT(app,authenticate, identity)
+
+
+
 
 
 api.add_resource(TestRestFul.Item, '/item/<string:name>') #http://127.0.0.1:5000/student/Rolf
